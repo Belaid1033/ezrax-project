df978b63ff588e2ad2bf5c7a08551972bdce6bd4
Belaid Ahouari <87024200+Belaid1033@users.noreply.github.com>
2025-06-08 12:28:00 +0100

    Add files via upload

---
+++ ezrax-agent/reporting/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Package de génération de rapports pour l'agent EZRAX IDS/IPS
+"""
+
+from .report_generator import ReportGenerator
+
+__all__ = ['ReportGenerator']
